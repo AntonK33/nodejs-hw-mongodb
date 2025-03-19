012684db7079daca3135d10d9892e1a4202aacf5
AntonK33 <antonkulackov@gmail.com>
2025-03-19 18:27:45 +0100

    2

--- src/server.js
+++ src/server.js
@@ -14,24 +14,24 @@ export const setupServer = () => {
        
     app.use(cors());
     app.use(express.json());
-    //     const logger = pino({
-    //     transport: {
-    //     target: "pino-pretty",
-    //     options: { colorize: true }
-    //     }
-    // });
-    // app.use(pinoHttp({ logger }));
+        const logger = pino({
+        transport: {
+        target: "pino-pretty",
+        options: { colorize: true }
+        }
+    });
+    app.use(pinoHttp({ logger }));
 
     app.use("/api/contacts", contactsRouter);
 
-   app.use((req, res, next) => {
-    res.status(404);
-    res.end();
+  app.use((req, res, next) => {
+    res.status(404).end();
 });
 
-// Обработчик ошибок (чтобы молча игнорировать любые ошибки)
+// Логируем ошибки, но не отправляем ответ клиенту
 app.use((err, req, res, next) => {
-    res.end();
+    req.log.error(err); // Используем pino для логирования ошибки
+    res.status(500).end();
 });
 
     // app.use((_, res, next) => {
