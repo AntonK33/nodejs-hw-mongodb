65f90a8d73166df1d75302fc717ec67fb3a69964
AntonK33 <antonkulackov@gmail.com>
2025-03-24 16:25:19 +0100

    2

--- src/server.js
+++ src/server.js
@@ -44,7 +44,7 @@ export const setupServer = () => {
      app.listen(PORT, () => {
         console.log(`🚀 Server is running on port: ${PORT}`);
          });
-    };
+   
     // mongoose.connect(MONGODB_URL)
     //  .then(() => {
     //     console.log('Database connection successful');
