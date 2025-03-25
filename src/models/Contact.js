24c9eeb51435a3b2517071f1b03c0a8cd406e764
AntonK33 <antonkulackov@gmail.com>
2025-03-25 19:58:44 +0100

    2

--- src/models/Contact.js
+++ src/models/Contact.js
@@ -16,12 +16,12 @@ const contactSchema = new Schema({
         type: Boolean,
         default: false,
     },
-    // contactType: {
-    //      type: String,
-    //     enum: ["work", "home", "personal"],
-    //   required: [true, "default"],
-    //   default: "personal",
-    // },
+    contactType: {
+         type: String,
+        enum: ["work", "home", "personal"],
+      required: [true, "default"],
+      default: "personal",
+    },
 }, { versionKey: false, timestamps: true });
 
 contactSchema.post("save", function (doc) {
