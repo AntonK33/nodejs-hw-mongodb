513d51b135fd26bb57008b0b2a16ff06e50daf7d
AntonK33 <antonkulackov@gmail.com>
2025-03-26 17:34:13 +0100

    2

--- src/controllers/contactControllers.js
+++ src/controllers/contactControllers.js
@@ -50,29 +50,29 @@ export const getOneContact = async (req, res, next) => {
 };
 
 export const addContact = async (req, res, next) => {
-  //   try {
-  //    const { error } = createContactSchema.validate(req.body);
-  //     if (error) {
-  //     return  next(createHttpError(400, error.message));
-  //     }
-  //     const result = await contactsServices.addContact({ ...req.body });
+    try {
+     const { error } = createContactSchema.validate(req.body);
+      if (error) {
+      return  next(createHttpError(400, error.message));
+      }
+      const result = await contactsServices.addContact({ ...req.body });
 
-  //    return res.json({
-  // 		status: 201,
-  // 		message: "Successfully created a contact!",
-  // 		data: result,
+     return res.json({
+  		status: 201,
+  		message: "Successfully created a contact!",
+  		data: result,
 
-  // });
-  //  } catch (error) {
-  //  return next(error);
-  //  }
-  const result = await contactsServices.addContact({ ...req.body });
-
-  return res.json({
-    status: 201,
-    message: 'Successfully created a contact!',
-    data: result,
   });
+   } catch (error) {
+   return next(error);
+   }
+  // const result = await contactsServices.addContact({ ...req.body });
+
+  // return res.json({
+  //   status: 201,
+  //   message: 'Successfully created a contact!',
+  //   data: result,
+  // });
 };
 
 export const updateContact = async (req, res, next) => {
