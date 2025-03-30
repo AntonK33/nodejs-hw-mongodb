2e318fb3202ef5a3ce2601ed77c055ac2e3cef41
AntonK33 <antonkulackov@gmail.com>
2025-03-30 21:01:10 +0200

    2

--- src/models/User.js
+++ src/models/User.js
@@ -2,7 +2,7 @@ import { Schema, model } from "mongoose";
 import { handleSaveError, setUpdateSettings } from "./hooks.js";
 
 const userSchema = new Schema({
-    name: {
+    username: {
     type: String,
     required: true,
   },
