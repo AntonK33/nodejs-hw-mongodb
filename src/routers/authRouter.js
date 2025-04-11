4b712e603f6ba1729438fd8e21d78ce71509fc3b
AntonK33 <antonkulackov@gmail.com>
2025-04-11 14:30:31 +0200

    ausenticate deleted

--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -11,7 +11,7 @@ const router = Router();
 
 router.post('/register',upload.none(), validateBody(userSignupSchema), authController.signup);
 router.post('/login',upload.none(), validateBody(userSigninSchema), authController.signin);
-router.post('/logout', authenticate, authController.signout);
+router.post('/logout', authController.signout);
 router.post(
   '/refresh',
   authenticate,
