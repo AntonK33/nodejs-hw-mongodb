4dd5a80bef54fc5a4927a94c51047a8d139c6a62
AntonK33 <antonkulackov@gmail.com>
2025-04-11 13:49:33 +0200

    2

--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -2,14 +2,13 @@ import { Router } from 'express';
 import authController from '../controllers/authController.js';
 import validateBody from '../middelwares/validateBody.js';
 import { userSignupSchema, userSigninSchema } from '../schemas/usersSchemas.js';
-import authenticate from '../middelwares/authenticate.js';
 
 const router = Router();
-
-router.post('/register', validateBody(userSignupSchema), authController.signup);
-router.post('/login', validateBody(userSigninSchema), authController.signin);
-router.post('/logout', authenticate, authController.signout);
 router.post('/refresh',
   authController.refreshUserSessionController,
 );
+router.post('/register', validateBody(userSignupSchema), authController.signup);
+router.post('/login', validateBody(userSigninSchema), authController.signin);
+router.post('/logout',  authController.signout);
+
 export default router;
