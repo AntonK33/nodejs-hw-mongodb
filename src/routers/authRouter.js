d9834b5121d7551a508cd18fc716dad066e24a7a
AntonK33 <antonkulackov@gmail.com>
2025-04-12 14:51:46 +0200

    upload folder deleted

--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -3,12 +3,12 @@ import authController from '../controllers/authController.js';
 import validateBody from '../middelwares/validateBody.js';
 import { userSignupSchema, userSigninSchema } from '../schemas/usersSchemas.js';
 import { requestResetEmailSchema, resetPasswordSchema } from "../validation/auth.js";
-import { upload } from '../middelwares/multer.js';
+
 
 const router = Router();
 
 router.post('/register', validateBody(userSignupSchema), authController.signup);
-router.post('/login', upload.none(), validateBody(userSigninSchema), authController.signin);
+router.post('/login', validateBody(userSigninSchema), authController.signin);
 router.post('/logout', authController.signout);
 router.post(
   '/refresh',
