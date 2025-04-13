5b38f63c32a968566689f8d4022a7a67b1bc9af1
AntonK33 <antonkulackov@gmail.com>
2025-04-13 21:30:57 +0200

    password-pwd

--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -20,7 +20,7 @@ router.post(
   authController.requestResetEmailController,
 );
 router.post(
-  '/reset-password',
+  '/reset-pwd',
   validateBody(resetPasswordSchema),
   authController.resetPasswordController,
 );
