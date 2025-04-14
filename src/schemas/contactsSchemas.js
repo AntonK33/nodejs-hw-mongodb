436227964339fd09b92192db487e95f5824a4ceb
AntonK33 <antonkulackov@gmail.com>
2025-04-14 20:01:16 +0200

    photo

--- src/schemas/contactsSchemas.js
+++ src/schemas/contactsSchemas.js
@@ -7,7 +7,7 @@ export const createContactSchema = Joi.object({
 
   phoneNumber: Joi.string().min(3).max(20).required(),
 
-  photo: Joi.any().optional().allow(null, ''),
+  photo: Joi.string(),
 
   isFavourite: Joi.boolean(),
 
@@ -24,7 +24,7 @@ export const updateContactSchema = Joi.object({
 
   phoneNumber: Joi.string(),
 
-  photo: Joi.any().optional().allow(null, ''),
+  photo: Joi.string(),
 
   isFavourite: Joi.boolean(),
 
